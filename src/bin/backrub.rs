4860ff54f613e34d64644c2124e37c9b1fea7bdc
Brolf <brolf@magheute.net>
2023-05-13 15:36:32 +0200

    removed old test binaries

--- src/bin/backrub.rs
+++ src/bin/backrub.rs
@@ -1,256 +1,261 @@
-use clap::{Arg, Command, ValueHint};
-use clap_complete::{generate, Shell};
-use log::{error, info, warn};
-use std::io;
-use std::path::{Path, PathBuf};
-use std::process::exit;
-//use serde::{Deserialize, Serialize};
-use backrub::*;
-
-const APPNAME: &str = "dedup";
-const TREE_EXTENSION: &str = "tree.json.zip";
-const CONFIG_EXTENSION: &str = "ini";
-
-// static INI_TEMPLATE: &'static str = include_str!("dedup-template.ini");
-
-fn default_conf_file() -> PathBuf {
-    match dirs::config_dir() {
-        Some(dir) => {
-            let mut path = dir;
-            path.push(APPNAME);
-            path.set_extension(CONFIG_EXTENSION);
-            path
-        }
-        None => {
-            let mut path = PathBuf::from(APPNAME);
-            path.set_extension(CONFIG_EXTENSION);
-            path
-        }
-    }
-}
-
-fn default_tree_file() -> PathBuf {
-    match dirs::data_local_dir() {
-        Some(dir) => {
-            let mut path = dir;
-            path.push(APPNAME);
-            path.set_extension(TREE_EXTENSION);
-            path
-        }
-        None => {
-            let mut path = PathBuf::from(APPNAME);
-            path.set_extension(TREE_EXTENSION);
-            path
-        }
-    }
-}
-
-struct Config {
-    treeFilePath: PathBuf,
-    followSymlinks: bool,
-}
-
-impl Config {
-    fn new() -> Self {
-        Config {
-            treeFilePath: PathBuf::new(),
-            followSymlinks: true,
-        }
-    }
-}
-
-fn build_cli() -> Command<'static> {
-    Command::new(APPNAME)
-        .subcommand_required(true)
-        .subcommand(
-            Command::new("config")
-                .long_flag("config")
-                .short_flag('c')
-                .about("Change or print the configuration"), //.arg(
-                                                             //    todo!("Todo");
-                                                             //),
-        )
-        .subcommand(
-            Command::new("completion")
-                .long_flag("completion")
-                .about("Print shell completions")
-                .arg(
-                    Arg::new("shell")
-                        .possible_values(Shell::possible_values())
-                        .help("The shell for witch the completions are generated")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            Command::new("print")
-                .long_flag("print")
-                .visible_long_flag_alias("ls")
-                .short_flag('p')
-                .about("Print duplicats in <tree> optionally restrict output to <path>")
-                .arg(
-                    Arg::new("tree")
-                        .value_hint(ValueHint::FilePath)
-                        .help("The file where the tree is stored in")
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("path")
-                        .help("The path results are filtered with")
-                        .value_hint(ValueHint::DirPath),
-                ),
-        )
-        .subcommand(
-            Command::new("analyze")
-                .long_flag("analyze")
-                .short_flag('a')
-                .about("Analyze <path> and update <tree>")
-                .arg(
-                    Arg::new("tree")
-                        .help("The file where the tree is stored in")
-                        .value_hint(ValueHint::FilePath)
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("path")
-                        .help("The path to analyze")
-                        .value_hint(ValueHint::DirPath)
-                        .required(true),
-                ),
-        )
-}
-
-#[allow(unreachable_code)]
-fn parse_config() {
-    let config_file_path: PathBuf;
-    let config_file_path_env = std::env::var("DEDUP_CONFIG_PATH");
-    if config_file_path_env.is_err() {
-        config_file_path = default_conf_file();
-    } else {
-        config_file_path = Path::new(&config_file_path_env.unwrap()).to_path_buf();
-    }
-
-    let config = Config::new();
-    if config_file_path.exists() {
-        match std::fs::read_to_string(config_file_path.clone()) {
-            Ok(config_file) => {
-                let mut ini = configparser::ini::Ini::new();
-                ini.read(config_file.clone());
-            }
-            Err(e) => {
-                error!(
-                    "Could not read cofig file {}:\n{}",
-                    config_file_path.display(),
-                    e
-                );
-                exit(1);
-            }
-        }
-    } else {
-        todo!("Load and save default config file")
-    }
-
-    // TODO Parse config file
-    todo!("Config and command line parsing is not implemented!");
-
-    println!("Would read {}", default_conf_file().display());
-    println!("Would save to {}", default_tree_file().display());
-
-    let matches = build_cli().get_matches();
-
-    match matches.subcommand() {
-        Some(("print", s_print)) => {
-            println!("Subcommand print was used: {:?}", s_print);
-            todo!()
-        }
-        Some(("analyze", s_ana)) => {
-            println!("Subcommand analyze was used: {:?}", s_ana);
-            todo!()
-        }
-        Some(("completion", s_comp)) => {
-            let shell = s_comp
-                .value_of_t::<Shell>("shell")
-                .unwrap_or_else(|e| e.exit());
-            generate(
-                shell,
-                &mut build_cli(),
-                build_cli().get_name().to_string(),
-                &mut io::stdout(),
-            );
-            exit(0)
-        }
-        _ => {
-            unreachable!("There should be no unimplemented subcommands")
-        }
-    }
-}
-use backrub::structs::*;
-use sled::{Error, IVec};
-use std::convert::TryInto;
-use once_cell::sync::OnceCell;
-
-
-static TEST: OnceCell<u64> = OnceCell::new();
-
 fn main() {
-    let config = sled::Config::new().temporary(true);
-    let db = config.open().unwrap();
-    let tree = db.open_tree(b"test").unwrap();
-    tree.insert(b"key", b"value");
-    tree.insert(b"k2",b"v2");
-    for t in tree.iter() {
-        match t {
-            Ok((k,v)) => {
-                println!("{:?} => {:?}", k, v);
-            }
-            Err(e) => {
-                println!("ERROR: {}", e);
-            }
-        }
-    }
 
-    TEST.set(64);
-    println!("{}", TEST.get().unwrap());
 }
 
-fn main2() {
-    env_logger::init();
 
-    let mut tree = DedupTree::new();
-    tree.update(".", true);
-
-    let json = tree.to_json();
-    let des_tree = DedupTree::from_json(&json).unwrap();
-
-    println!();
-    println!("json: {}", json);
-
-    println!();
-    println!("tree sizes: {}/{}", tree.len_unique(), tree.len_paths());
-    println!("found {} unique duplicates", tree.get_duplicates().len());
-
-    println!();
-    println!(
-        "des_tree sizes: {}/{}",
-        des_tree.len_unique(),
-        des_tree.len_paths()
-    );
-    println!(
-        "found {} unique duplicates",
-        des_tree.get_duplicates().len()
-    );
-
-    println!();
-    println!("tree == des_tree: {}", tree == des_tree);
-
-    tree.delete_dir(".").unwrap();
-    println!();
-    println!("deleted tree size: {}", tree.len_paths());
-
-    let bencoded: Vec<u8> = bincode::serialize(&des_tree).unwrap();
-    let bdecoded: DedupTree = bincode::deserialize(&bencoded[..]).unwrap();
-
-    println!();
-    println!("bindecoded == des_tree: {}", bdecoded == des_tree);
-
-    //   parse_config();
-}
+// use clap::{Arg, Command, ValueHint};
+// use clap_complete::{generate, Shell};
+// use log::{error, info, warn};
+// use std::io;
+// use std::path::{Path, PathBuf};
+// use std::process::exit;
+// //use serde::{Deserialize, Serialize};
+// use backrub::*;
+// 
+// const APPNAME: &str = "dedup";
+// const TREE_EXTENSION: &str = "tree.json.zip";
+// const CONFIG_EXTENSION: &str = "ini";
+// 
+// // static INI_TEMPLATE: &'static str = include_str!("dedup-template.ini");
+// 
+// fn default_conf_file() -> PathBuf {
+//     match dirs::config_dir() {
+//         Some(dir) => {
+//             let mut path = dir;
+//             path.push(APPNAME);
+//             path.set_extension(CONFIG_EXTENSION);
+//             path
+//         }
+//         None => {
+//             let mut path = PathBuf::from(APPNAME);
+//             path.set_extension(CONFIG_EXTENSION);
+//             path
+//         }
+//     }
+// }
+// 
+// fn default_tree_file() -> PathBuf {
+//     match dirs::data_local_dir() {
+//         Some(dir) => {
+//             let mut path = dir;
+//             path.push(APPNAME);
+//             path.set_extension(TREE_EXTENSION);
+//             path
+//         }
+//         None => {
+//             let mut path = PathBuf::from(APPNAME);
+//             path.set_extension(TREE_EXTENSION);
+//             path
+//         }
+//     }
+// }
+// 
+// struct Config {
+//     treeFilePath: PathBuf,
+//     followSymlinks: bool,
+// }
+// 
+// impl Config {
+//     fn new() -> Self {
+//         Config {
+//             treeFilePath: PathBuf::new(),
+//             followSymlinks: true,
+//         }
+//     }
+// }
+// 
+// fn build_cli() -> Command<'static> {
+//     Command::new(APPNAME)
+//         .subcommand_required(true)
+//         .subcommand(
+//             Command::new("config")
+//                 .long_flag("config")
+//                 .short_flag('c')
+//                 .about("Change or print the configuration"), //.arg(
+//                                                              //    todo!("Todo");
+//                                                              //),
+//         )
+//         .subcommand(
+//             Command::new("completion")
+//                 .long_flag("completion")
+//                 .about("Print shell completions")
+//                 .arg(
+//                     Arg::new("shell")
+//                         .possible_values(Shell::possible_values())
+//                         .help("The shell for witch the completions are generated")
+//                         .required(true),
+//                 ),
+//         )
+//         .subcommand(
+//             Command::new("print")
+//                 .long_flag("print")
+//                 .visible_long_flag_alias("ls")
+//                 .short_flag('p')
+//                 .about("Print duplicats in <tree> optionally restrict output to <path>")
+//                 .arg(
+//                     Arg::new("tree")
+//                         .value_hint(ValueHint::FilePath)
+//                         .help("The file where the tree is stored in")
+//                         .required(true),
+//                 )
+//                 .arg(
+//                     Arg::new("path")
+//                         .help("The path results are filtered with")
+//                         .value_hint(ValueHint::DirPath),
+//                 ),
+//         )
+//         .subcommand(
+//             Command::new("analyze")
+//                 .long_flag("analyze")
+//                 .short_flag('a')
+//                 .about("Analyze <path> and update <tree>")
+//                 .arg(
+//                     Arg::new("tree")
+//                         .help("The file where the tree is stored in")
+//                         .value_hint(ValueHint::FilePath)
+//                         .required(true),
+//                 )
+//                 .arg(
+//                     Arg::new("path")
+//                         .help("The path to analyze")
+//                         .value_hint(ValueHint::DirPath)
+//                         .required(true),
+//                 ),
+//         )
+// }
+// 
+// #[allow(unreachable_code)]
+// fn parse_config() {
+//     let config_file_path: PathBuf;
+//     let config_file_path_env = std::env::var("DEDUP_CONFIG_PATH");
+//     if config_file_path_env.is_err() {
+//         config_file_path = default_conf_file();
+//     } else {
+//         config_file_path = Path::new(&config_file_path_env.unwrap()).to_path_buf();
+//     }
+// 
+//     let config = Config::new();
+//     if config_file_path.exists() {
+//         match std::fs::read_to_string(config_file_path.clone()) {
+//             Ok(config_file) => {
+//                 let mut ini = configparser::ini::Ini::new();
+//                 ini.read(config_file.clone());
+//             }
+//             Err(e) => {
+//                 error!(
+//                     "Could not read cofig file {}:\n{}",
+//                     config_file_path.display(),
+//                     e
+//                 );
+//                 exit(1);
+//             }
+//         }
+//     } else {
+//         todo!("Load and save default config file")
+//     }
+// 
+//     // TODO Parse config file
+//     todo!("Config and command line parsing is not implemented!");
+// 
+//     println!("Would read {}", default_conf_file().display());
+//     println!("Would save to {}", default_tree_file().display());
+// 
+//     let matches = build_cli().get_matches();
+// 
+//     match matches.subcommand() {
+//         Some(("print", s_print)) => {
+//             println!("Subcommand print was used: {:?}", s_print);
+//             todo!()
+//         }
+//         Some(("analyze", s_ana)) => {
+//             println!("Subcommand analyze was used: {:?}", s_ana);
+//             todo!()
+//         }
+//         Some(("completion", s_comp)) => {
+//             let shell = s_comp
+//                 .value_of_t::<Shell>("shell")
+//                 .unwrap_or_else(|e| e.exit());
+//             generate(
+//                 shell,
+//                 &mut build_cli(),
+//                 build_cli().get_name().to_string(),
+//                 &mut io::stdout(),
+//             );
+//             exit(0)
+//         }
+//         _ => {
+//             unreachable!("There should be no unimplemented subcommands")
+//         }
+//     }
+// }
+// use backrub::structs::*;
+// use sled::{Error, IVec};
+// use std::convert::TryInto;
+// use once_cell::sync::OnceCell;
+// 
+// 
+// static TEST: OnceCell<u64> = OnceCell::new();
+// 
+// fn main() {
+//     let config = sled::Config::new().temporary(true);
+//     let db = config.open().unwrap();
+//     let tree = db.open_tree(b"test").unwrap();
+//     tree.insert(b"key", b"value");
+//     tree.insert(b"k2",b"v2");
+//     for t in tree.iter() {
+//         match t {
+//             Ok((k,v)) => {
+//                 println!("{:?} => {:?}", k, v);
+//             }
+//             Err(e) => {
+//                 println!("ERROR: {}", e);
+//             }
+//         }
+//     }
+// 
+//     TEST.set(64);
+//     println!("{}", TEST.get().unwrap());
+// }
+// 
+// fn main2() {
+//     env_logger::init();
+// 
+//     let mut tree = DedupTree::new();
+//     tree.update(".", true);
+// 
+//     let json = tree.to_json();
+//     let des_tree = DedupTree::from_json(&json).unwrap();
+// 
+//     println!();
+//     println!("json: {}", json);
+// 
+//     println!();
+//     println!("tree sizes: {}/{}", tree.len_unique(), tree.len_paths());
+//     println!("found {} unique duplicates", tree.get_duplicates().len());
+// 
+//     println!();
+//     println!(
+//         "des_tree sizes: {}/{}",
+//         des_tree.len_unique(),
+//         des_tree.len_paths()
+//     );
+//     println!(
+//         "found {} unique duplicates",
+//         des_tree.get_duplicates().len()
+//     );
+// 
+//     println!();
+//     println!("tree == des_tree: {}", tree == des_tree);
+// 
+//     tree.delete_dir(".").unwrap();
+//     println!();
+//     println!("deleted tree size: {}", tree.len_paths());
+// 
+//     let bencoded: Vec<u8> = bincode::serialize(&des_tree).unwrap();
+//     let bdecoded: DedupTree = bincode::deserialize(&bencoded[..]).unwrap();
+// 
+//     println!();
+//     println!("bindecoded == des_tree: {}", bdecoded == des_tree);
+// 
+//     //   parse_config();
+// }
--- src/bin/chuncker_test.rs
+++ src/bin/chuncker_test.rs
@@ -1,116 +1,120 @@
-use hash_roll::fastcdc;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-//use std::prelude::*;
-use hash_roll::ChunkIncr;
-use memmap::Mmap;
-//use dedup::*;
+fn main(){
 
-fn get_first_chunks(buff: &[u8]) -> (Vec<Vec<u8>>, Vec<u8>) {
-    let chunk_iter = fastcdc::FastCdcIncr::default();
-    let mut iter_slices = chunk_iter.iter_slices_strict(buff);
-
-    let mut chunks = Vec::<Vec<u8>>::new();
-
-    while let Some(chunk) = iter_slices.next() {
-        chunks.push(chunk.to_vec());
-    }
-
-    let rest = iter_slices.by_ref().take_rem().to_vec();
-
-    (chunks, rest)
 }
 
-fn get_chunks(buff: &[u8], rest: &[u8]) -> (Vec<Vec<u8>>, Vec<u8>) {
-    get_first_chunks(&[rest, buff].concat())
-}
-
-fn chunk_using_buf_reader(fname: String) -> Vec<Vec<u8>> {
-    let f = File::open(fname).unwrap();
-    let fsize = f.metadata().unwrap().len();
-    let mut breader = BufReader::with_capacity(1usize << 20, f);
-
-    let fbuff = breader.fill_buf().unwrap();
-    let fbuff_len = fbuff.len();
-
-    let mut cnew: Vec<Vec<u8>>;
-
-    let (mut chunks, mut rest) = get_first_chunks(fbuff);
-    breader.consume(fbuff_len);
-
-    while let Ok(buff) = breader.fill_buf() {
-        let buff_len = buff.len();
-        if buff_len == 0 {
-            break;
-        }
-        (cnew, rest) = get_chunks(buff, &rest[..]);
-        chunks.append(&mut cnew);
-        breader.consume(buff_len);
-    }
-    chunks.push(rest.to_vec());
-
-    let mut sizesum: usize = 0;
-    let anzchunks = chunks.len();
-    for chunk in chunks.clone() {
-        sizesum += chunk.len();
-    }
-
-    println!("chunked size total: {}", sizesum);
-    println!("file size metadata: {}", fsize);
-    println!("anz chunks total: {}", anzchunks);
-
-    chunks
-}
-
-fn chunk_using_mmap(fname: String) -> Vec<Vec<u8>> {
-    let f = File::open(fname).unwrap();
-    let fsize = f.metadata().unwrap().len();
-
-    let chunk_iter = fastcdc::FastCdcIncr::default();
-    let mmap = unsafe { Mmap::map(&f).unwrap() };
-
-    let mut chunks = Vec::<Vec<u8>>::new();
-
-    for chunk in chunk_iter.iter_slices(&mmap[..]) {
-        let cv = chunk.to_vec();
-        chunks.push(cv);
-    }
-
-    let mut sizesum: usize = 0;
-    let anzchunks = chunks.len();
-    for chunk in chunks.clone() {
-        sizesum += chunk.len();
-    }
-
-    println!("chunked size total: {}", sizesum);
-    println!("file size metadata: {}", fsize);
-    println!("anz chunks total: {}", anzchunks);
-
-    chunks
-}
-
-fn main() {
-    let fname = std::env::args().nth(1).expect("no filename given");
-
-    println!();
-    println!("########################################################");
-    println!("############## BufReader ###############################");
-    println!("########################################################");
-    let c1 = chunk_using_buf_reader(fname.clone());
-
-    println!();
-    println!("########################################################");
-    println!("############## MMAP ####################################");
-    println!("########################################################");
-    let c2 = chunk_using_mmap(fname);
-
-    println!();
-    println!("########################################################");
-    println!("############## BufReader == MMAP #######################");
-    if c1 == c2 {
-        println!("#################### TRUE ##############################");
-    } else {
-        println!("#################### FALSE #############################");
-    }
-    println!("########################################################");
-}
+// use hash_roll::fastcdc;
+// use std::fs::File;
+// use std::io::{BufRead, BufReader};
+// //use std::prelude::*;
+// use hash_roll::ChunkIncr;
+// use memmap::Mmap;
+// //use dedup::*;
+// 
+// fn get_first_chunks(buff: &[u8]) -> (Vec<Vec<u8>>, Vec<u8>) {
+//     let chunk_iter = fastcdc::FastCdcIncr::default();
+//     let mut iter_slices = chunk_iter.iter_slices_strict(buff);
+// 
+//     let mut chunks = Vec::<Vec<u8>>::new();
+// 
+//     while let Some(chunk) = iter_slices.next() {
+//         chunks.push(chunk.to_vec());
+//     }
+// 
+//     let rest = iter_slices.by_ref().take_rem().to_vec();
+// 
+//     (chunks, rest)
+// }
+// 
+// fn get_chunks(buff: &[u8], rest: &[u8]) -> (Vec<Vec<u8>>, Vec<u8>) {
+//     get_first_chunks(&[rest, buff].concat())
+// }
+// 
+// fn chunk_using_buf_reader(fname: String) -> Vec<Vec<u8>> {
+//     let f = File::open(fname).unwrap();
+//     let fsize = f.metadata().unwrap().len();
+//     let mut breader = BufReader::with_capacity(1usize << 20, f);
+// 
+//     let fbuff = breader.fill_buf().unwrap();
+//     let fbuff_len = fbuff.len();
+// 
+//     let mut cnew: Vec<Vec<u8>>;
+// 
+//     let (mut chunks, mut rest) = get_first_chunks(fbuff);
+//     breader.consume(fbuff_len);
+// 
+//     while let Ok(buff) = breader.fill_buf() {
+//         let buff_len = buff.len();
+//         if buff_len == 0 {
+//             break;
+//         }
+//         (cnew, rest) = get_chunks(buff, &rest[..]);
+//         chunks.append(&mut cnew);
+//         breader.consume(buff_len);
+//     }
+//     chunks.push(rest.to_vec());
+// 
+//     let mut sizesum: usize = 0;
+//     let anzchunks = chunks.len();
+//     for chunk in chunks.clone() {
+//         sizesum += chunk.len();
+//     }
+// 
+//     println!("chunked size total: {}", sizesum);
+//     println!("file size metadata: {}", fsize);
+//     println!("anz chunks total: {}", anzchunks);
+// 
+//     chunks
+// }
+// 
+// fn chunk_using_mmap(fname: String) -> Vec<Vec<u8>> {
+//     let f = File::open(fname).unwrap();
+//     let fsize = f.metadata().unwrap().len();
+// 
+//     let chunk_iter = fastcdc::FastCdcIncr::default();
+//     let mmap = unsafe { Mmap::map(&f).unwrap() };
+// 
+//     let mut chunks = Vec::<Vec<u8>>::new();
+// 
+//     for chunk in chunk_iter.iter_slices(&mmap[..]) {
+//         let cv = chunk.to_vec();
+//         chunks.push(cv);
+//     }
+// 
+//     let mut sizesum: usize = 0;
+//     let anzchunks = chunks.len();
+//     for chunk in chunks.clone() {
+//         sizesum += chunk.len();
+//     }
+// 
+//     println!("chunked size total: {}", sizesum);
+//     println!("file size metadata: {}", fsize);
+//     println!("anz chunks total: {}", anzchunks);
+// 
+//     chunks
+// }
+// 
+// fn main() {
+//     let fname = std::env::args().nth(1).expect("no filename given");
+// 
+//     println!();
+//     println!("########################################################");
+//     println!("############## BufReader ###############################");
+//     println!("########################################################");
+//     let c1 = chunk_using_buf_reader(fname.clone());
+// 
+//     println!();
+//     println!("########################################################");
+//     println!("############## MMAP ####################################");
+//     println!("########################################################");
+//     let c2 = chunk_using_mmap(fname);
+// 
+//     println!();
+//     println!("########################################################");
+//     println!("############## BufReader == MMAP #######################");
+//     if c1 == c2 {
+//         println!("#################### TRUE ##############################");
+//     } else {
+//         println!("#################### FALSE #############################");
+//     }
+//     println!("########################################################");
+// }
